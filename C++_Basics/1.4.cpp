0bc15547465ab955131856e33c0e1c1fc2e8250b
methatsit <mefkitg@gmail.com>
2025-11-27 20:16:54 -0600

    Daily Auto-Backup: 2025-11-27 20:16

--- C++_Basics/1.4.cpp
+++ C++_Basics/1.4.cpp
@@ -33,9 +33,37 @@ int main()
     	// Initializing an int with fractional value 4.5 requires the compiler to convert 4.5 to a value an int can hold.
     	// Such a conversion is a narrowing conversion, since the fractional part of the value will be lost.
 
-    	// int w1 { 4.5 }; // compile error: list-init does not allow narrowing conversion
+    	//int w1 { 4 } ; // compile error: list-init does not allow narrowing conversion
 
-    	int w2 = 4.5;   // compiles: w2 copy-initialized to value 4
-    	int w3 (4.5);   // compiles: w3 direct-initialized to value 4
+	//std:: cout << "\n" << w1;
+    	// int w2 = 4.5;   // compiles: w2 copy-initialized to value 4
+    	// int w3 (4.5);   // compiles: w3 direct-initialized to value 4
+	// int width {};	   // Value-initialization / zero-initialization to value 0
+	
+	// When should I initialize with {0} vs {}?
+	// int x { 0 };	   // direct-list-initialization with initial value 0	
+	// std::cout << x << "\n";	   // we're using that 0 value here
+
+	// int y {};	   // value initialization
+	// std::cin >> y;	   // we're immediately replacing that value so an explicit 0 would be meaningless	
+	
+	// double pi { 4.14159 };
+	// double gravity { 9.8 };
+	// double phi { 1.61803 };
+
+	// std::cout << pi << '\n';	// pi is used
+	// std::cout << phi << '\n';	// phi is used
+	
+	// The compiler will likely complain about gravity being defined but unused
+	
+	[[maybe_unused]] double pi { 3.14159 };  // Don't complain if pi is unused
+    	[[maybe_unused]] double gravity { 9.8 }; // Don't complain if gravity is unused
+    	[[maybe_unused]] double phi { 1.61803 }; // Don't complain if phi is unused
+	
+	std::cout << pi << '\n';
+    	std::cout << phi << '\n';
+
+	// The compiler will no longer warn about gravity not being used
+
 	return 0;
 }
---
+++ C++_Basics/1.5/1.5.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // for std::cout
+
+int main()
+{
+    std::cout << "Hello" << " world!";
+    
+    int x{ 5 };
+    std::cout << "\nx is equal to: " << x << std::endl;
+
+    std::cout << "Hi!" << std::endl;
+    std::cout <<  "My name is Alex." << std::endl;
+    return 0;
+}
